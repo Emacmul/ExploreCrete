f372380d842237cdc6e89b226bdbea01dadc5e84
enda <enda@magicalcrete.com>
2026-07-14 18:24:10 +0000

    File changes

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -68,7 +68,7 @@ export default function Login() {
             {error && (
               <div className="flex items-start gap-2 text-sm text-red-600 bg-red-50 rounded-lg p-3">
                 <AlertCircle className="w-4 h-4 mt-0.5 shrink-0" />
-                <span>{error}</span>
+                <span dangerouslySetInnerHTML={{ __html: error }} />
               </div>
             )}
 
